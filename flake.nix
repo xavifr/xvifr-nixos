3bd5cf18c95613f91d990b0a874ca6cb4674606e
Xavier Franquet <xavier@franquet.es>
2025-05-30 00:12:52 +0200

    fix hostname for hostmanager in nixos-vm

--- flake.nix
+++ flake.nix
@@ -82,7 +82,7 @@
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
-      "xavier@vm" = home-manager.lib.homeManagerConfiguration {
+      "xavier@nixos-vm" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         modules = [
