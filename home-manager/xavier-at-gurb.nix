d7d2252ee736a958ae8998c469b5e3a9263e01c4
Xavier Franquet <xavier@franquet.es>
2025-05-30 00:10:28 +0200

    add pkg?

--- home-manager/xavier-at-gurb.nix
+++ home-manager/xavier-at-gurb.nix
@@ -51,7 +51,10 @@
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
+  home.packages = with pkgs; [
+    chromium
+    thunderbird
+  ];
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
