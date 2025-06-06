1cf0c39f2cf01c251dd0e5a2d468e0a384934f64
Xavier Franquet <xavier@franquet.es>
2025-06-06 10:35:05 +0200

    simplify

--- modules/home-manager/uping.nix
+++ modules/home-manager/uping.nix
@@ -10,30 +10,25 @@ with lib;
 let
   cfg = config.modules.uping;
 
-  # Fetch the uping repository from GitHub (using latest commit hash)
-  # To update: get the latest commit hash from https://github.com/xavifr/uping/commits/main
-  # and update both the URL and sha256 hash
-  upingSrc = builtins.fetchTarball {
-    url = "https://github.com/xavifr/uping/archive/8cef5a4f0af838498be669bed1c6bf5f9e48694f.tar.gz";
-    sha256 = "0arqikz5b9hmw3nyjwfz53gr0l1815b5xk3c04jv7kdgmd5q83q1";
-  };
+  # Fetch and import the uping flake directly
+  upingFlake = import "${
+    builtins.fetchTarball {
+      url = "https://github.com/xavifr/uping/archive/8cef5a4f0af838498be669bed1c6bf5f9e48694f.tar.gz";
+      sha256 = "0arqikz5b9hmw3nyjwfz53gr0l1815b5xk3c04jv7kdgmd5q83q1";
+    }
+  }/flake.nix";
 
-  # Import the uping flake
-  upingFlake = import "${upingSrc}/flake.nix";
-
-  # Prepare the inputs that the flake expects
-  inputs = {
-    nixpkgs = {
-      lib = pkgs.lib;
-      legacyPackages.${pkgs.system} = pkgs;
-    };
-  };
-
-  # Evaluate the flake outputs function with a fixed-point to resolve self-reference
-  upingOutputs = lib.fix (self: upingFlake.outputs (inputs // { inherit self; }));
-
-  # Get the uping package
-  upingPackage = upingOutputs.packages.${pkgs.system}.default;
+  # Evaluate the flake outputs with nixpkgs input
+  upingOutputs = lib.fix (
+    self:
+    upingFlake.outputs {
+      inherit self;
+      nixpkgs = {
+        lib = pkgs.lib;
+        legacyPackages.${pkgs.system} = pkgs;
+      };
+    }
+  );
 in
 {
   options.modules.uping = {
@@ -41,6 +36,6 @@ in
   };
 
   config = mkIf cfg.enable {
-    home.packages = [ upingPackage ];
+    home.packages = [ upingOutputs.packages.${pkgs.system}.default ];
   };
 }
--- scripts/update-uping.sh
+++ scripts/update-uping.sh
@@ -15,9 +15,9 @@ TARBALL_URL="https://github.com/xavifr/uping/archive/${LATEST_COMMIT}.tar.gz"
 SHA256=$(nix-prefetch-url --unpack "$TARBALL_URL")
 echo "SHA256: $SHA256"
 
-# Update the uping.nix file
+# Update the uping.nix file (new simplified structure)
 echo "Updating modules/home-manager/uping.nix..."
-sed -i "s|url = \"https://github.com/xavifr/uping/archive/[^\"]*\";|url = \"$TARBALL_URL\";|" modules/home-manager/uping.nix
+sed -i "s|https://github.com/xavifr/uping/archive/[^\"]*\.tar\.gz|$TARBALL_URL|" modules/home-manager/uping.nix
 sed -i "s|sha256 = \"[^\"]*\";|sha256 = \"$SHA256\";|" modules/home-manager/uping.nix
 
 echo "✅ Updated uping module to commit $LATEST_COMMIT"
