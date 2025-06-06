ffcc285bd4a6168d2186e155e9b8ddae220bb0a4
Xavier Franquet <xavier@franquet.es>
2025-06-06 13:52:58 +0200

    plasma-manager

--- modules/home-manager/plasma.nix
+++ modules/home-manager/plasma.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 
@@ -8,87 +9,114 @@ with lib;
 
 let
   cfg = config.modules.plasma;
+
+  # Fetch plasma-manager
+  plasma-manager = builtins.fetchTarball {
+    url = "https://github.com/pjones/plasma-manager/archive/trunk.tar.gz";
+    sha256 = "05gw226063jbklfgcyr01a04278v7shn8a4imjg47rdzgsqf68fn";
+  };
 in
 {
+  imports = [
+    (plasma-manager + "/modules")
+  ];
+
   options.modules.plasma = {
     enable = mkEnableOption "KDE Plasma customization";
   };
 
   config = mkIf cfg.enable {
-    home.packages = [
+    # Enable plasma-manager
+    programs.plasma = {
+      enable = true;
+
+      # Workspace settings
+      workspace = {
+        lookAndFeel = "org.kde.breezedark.desktop";
+        colorScheme = "BreezeDark";
+        cursor.theme = "breeze_cursors";
+        iconTheme = "breeze-dark";
+        wallpaper = builtins.fetchurl {
+          url = "https://raw.githubusercontent.com/KDE/plasma-workspace-wallpapers/eb98b8b6a9e2a22a9071bb72ab3f9771e08033ce/ScarletTree/contents/images_dark/5120x2880.png";
+          sha256 = "0aj1gm1c6n9vj8gni4dgq9h1d8rj36h9qwy8r4l7wf0wm3xxzn13";
+        };
+      };
+
+      # Fonts configuration
+      fonts = {
+        general = {
+          family = "Noto Sans";
+          pointSize = 10;
+        };
+        fixedWidth = {
+          family = "Hack";
+          pointSize = 10;
+        };
+      };
+
+      # Shortcuts
+      shortcuts = {
+        Ghostty = {
+          "Launch" = [
+            "Ctrl+Alt+T"
+          ];
+        };
+      };
+
+      # Panel configuration
+      panels = [
+        {
+          location = "bottom";
+          screen = "all";
+          widgets = [
+            "org.kde.plasma.kickoff"
+            {
+              name = "org.kde.plasma.pager";
+              config = {
+                General.showOnlyCurrentScreen = true;
+              };
+            }
+            {
+              name = "org.kde.plasma.taskmanager";
+              config = {
+                General = {
+                  middleClickAction = "ToggleGrouping";
+                  groupingStrategy = 1;
+                  iconSpacing = 0;
+                  showOnlyCurrentScreen = true;
+                };
+              };
+            }
+            "org.kde.plasma.marginsseparator"
+            {
+              name = "org.kde.plasma.systemtray";
+              config = {
+                icons.spacing = "small";
+              };
+            }
+            {
+              name = "org.kde.plasma.digitalclock";
+              config = {
+                Appearance = {
+                  firstDayOfWeek = "monday";
+                  showWeekNumbers = true;
+                  showSeconds = "Always";
+                };
+              };
+            }
+            "org.kde.plasma.showdesktop"
+          ];
+        }
+      ];
+
+      # Window rules
+      window-rules = [
+      ];
+    };
+
+    # Additional packages for Plasma
+    home.packages = with pkgs; [
+      nil
     ];
-
-    programs.starship = {
-      enable = true;
-      settings = {
-        add_newline = true;
-        command_timeout = 1300;
-        scan_timeout = 50;
-        format = "[░▒▓](#a3aed2)[  ](bg:#a3aed2 fg:#090c0c)[](bg:#769ff0 fg:#a3aed2)$directory[](fg:#769ff0 bg:#394260)$git_branch$git_status[](fg:#394260 bg:#212736)$nodejs$golang$python$kubernetes[](fg:#212736 bg:#1d2230)$time[](fg:#1d2230)\n$character";
-        directory = {
-          style = "fg:#e3e5e5 bg:#769ff0";
-          format = "[ $path ]($style)";
-          truncation_length = 3;
-          truncation_symbol = "…/";
-        };
-        git_branch = {
-          symbol = "";
-          style = "bg:#394260";
-          format = "[[ $symbol $branch ](fg:#769ff0 bg:#394260)]($style)";
-        };
-
-        git_status = {
-          style = "bg:#394260";
-          format = "[[($all_status$ahead_behind )](fg:#769ff0 bg:#394260)]($style)";
-        };
-
-        nodejs = {
-          symbol = "";
-          style = "bg:#212736";
-          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-        };
-
-        golang = {
-          symbol = "";
-          style = "bg:#212736";
-          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-        };
-
-        python = {
-          symbol = "";
-          style = "bg:#212736";
-          format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
-        };
-
-        time = {
-          disabled = false; # Nix boolean value
-          time_format = "%R";
-          style = "bg:#1d2230";
-          format = "[[  $time ](fg:#a0a9cb bg:#1d2230)]($style)";
-        };
-        character = {
-          success_symbol = "[](bold green) ";
-          error_symbol = "[✗](bold red) ";
-        };
-      };
-    };
-
-    programs.ghostty = {
-      enable = true;
-      enableFishIntegration = true;
-      settings = {
-        background-opacity = 0.95;
-        background-blur = 40;
-        shell-integration = "fish";
-        command = "fish";
-        keybind = [
-          "ctrl+backspace=text:\\x15"
-        ];
-      };
-    };
-
-    programs.fish = {
-      enable = true;
-    };
   };
 }
